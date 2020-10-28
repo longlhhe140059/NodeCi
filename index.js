cecf3213bbfd585155d26b141beeb654d3ad036d
longlhhe140059 <73572102+longlhhe140059@users.noreply.github.com>
2020-10-28 11:29:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,8 +38,10 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
-
+ 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
+
+//dsadasdsadaddaddsdsad
